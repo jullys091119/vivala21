20dd612f20bde018e066e27abafb1e979ed8053f
Julian Ontiveros <arcontiveros@gmail.com>
2025-04-30 19:30:22 -0600

    add type meta

--- app/noticias/[id]/page.js
+++ app/noticias/[id]/page.js
@@ -1,11 +1,11 @@
-
 import { fetchNoticia } from '@/utils/fetchNoticia';
 import NewsClient from '@/app/noticias/newsClient';
-
 
 export async function generateMetadata({ params }) {
   const noticia = await fetchNoticia(params.id);
   const yoast = noticia?.yoast_head_json || {};
+
+  const url = `${process.env.NEXT_PUBLIC_SITE_URL}/noticias/${params.id}`;
 
   return {
     title: yoast.title || noticia?.title?.rendered,
@@ -13,7 +13,11 @@ export async function generateMetadata({ params }) {
     openGraph: {
       title: yoast['og:title'] || noticia?.title?.rendered,
       description: yoast['og:description'] || noticia?.excerpt?.rendered,
-      images: [yoast['og:image'] || noticia?._embedded?.['wp:featuredmedia']?.[0]?.source_url || ''],
+      images: [
+        yoast['og:image'] || noticia?._embedded?.['wp:featuredmedia']?.[0]?.source_url || '',
+      ],
+      url: url,  // Aquí agregamos la propiedad og:url
+      type: 'article', // Aquí agregamos la propiedad og:type
     },
   };
 }

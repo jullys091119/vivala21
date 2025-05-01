8e0412e1f1fb538ce02759aee3c879a0dc84ffe8
Julian Ontiveros <arcontiveros@gmail.com>
2025-04-30 18:03:24 -0600

    nuevo-1

--- app/noticias/[id]/page.js
+++ app/noticias/[id]/page.js
@@ -1,35 +1,23 @@
-// pages/noticias/[id].js
+// app/noticias/[id]/page.js
 import { fetchNoticiaData } from '@/lib/fetchNoticia';
 import NewsClient from '@/app/noticias/newsClient';
 
-export async function getStaticPaths() {
-  // Traemos todos los IDs de las noticias desde la API
+export async function generateStaticParams() {
+  // Traemos los IDs de todas las noticias disponibles.
   const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}wp/v2/posts`);
   const posts = await res.json();
 
-  // Generamos las rutas para todas las noticias
-  const paths = posts.map((post) => ({
-    params: { id: post.id.toString() },
+  // Generamos los parámetros de ruta estáticos para todas las noticias
+  return posts.map((post) => ({
+    id: post.id.toString(),
   }));
-
-  return {
-    paths,
-    fallback: 'blocking',  // 'blocking' asegura que las páginas se generen en el primer acceso si no existen aún
-  };
 }
 
-export async function getStaticProps({ params }) {
+export default async function NewsPage({ params }) {
   const { id } = params;
+
+  // Usamos fetch para obtener los datos de la noticia con el ID
   const noticia = await fetchNoticiaData(id);
 
-  return {
-    props: {
-      noticia,
-    },
-    revalidate: 60,  // Se vuelve a generar la página después de 60 segundos si hay cambios
-  };
-}
-
-export default function NewsPage({ noticia }) {
   return <NewsClient noticia={noticia} />;
 }

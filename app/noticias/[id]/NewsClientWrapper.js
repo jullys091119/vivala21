71e15b297e2d10c5e24afd6db415f82e70733712
Julian Ontiveros <arcontiveros@gmail.com>
2025-05-01 20:55:57 -0600

    Fix: corregido uso de dynamic con ssr:false en Server Component

--- app/noticias/[id]/NewsClientWrapper.js
+++
@@ -1,9 +0,0 @@
-// app/noticias/[id]/NewsClientWrapper.js
-'use client';
-
-import dynamic from 'next/dynamic';
-
-
-export default function NewsClientWrapper({ noticia }) {
-  return <NewsClient noticia={noticia} />;
-}
